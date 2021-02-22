760723d764c9ff9c17e710745bd91124dd8d4be2
Geronimo.Somma <sommagero@gsoft.com.uy>
2021-02-22 10:32:17 -0300

    agrego wcfLogistica

---
+++ BiosLogistica/wcfLogistica/App_Code/IService.cs
@@ -0,0 +1,64 @@
+﻿using EntidadesCompartidas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IService
+{
+    #region LogicaUsuario
+
+    [OperationContract]
+    void AltaUsuario(Usuario usuario, Empleado usLog);
+
+    [OperationContract]
+    void BajaUsuario(Usuario usuario, Empleado usLog);
+
+    [OperationContract]
+    Usuario BuscarUsuario(string logueo, Empleado usLog);
+
+    [OperationContract]
+    Usuario LogueoUsuario(string logueo, string contraseana);
+
+    [OperationContract]
+    void ModificarUsuario(Usuario usuario, Empleado usLog);
+
+    [OperationContract]
+    List<Empresa> ListarEmpresas(Empleado usuarioLogueado);
+
+    #endregion
+
+    #region LogicaPaquete
+
+    [OperationContract]
+    void AltaPaquete(Paquete paquete, Empleado usLog);
+
+    [OperationContract]
+    Paquete BuscarPaquete(int codigo, Empleado usLog);
+
+    [OperationContract]
+    List<Paquete> ListadoPaquetesSinSolicitud(Empleado usLog);
+
+    #endregion
+
+    #region LogicaSolicitud
+
+    [OperationContract]
+    void AltaSolicitud(Solicitud solicitud, Empleado usLog);
+
+    [OperationContract]
+    void ModificarEstadoSolicitud(Solicitud solicitud, Empleado usLog);
+
+    [OperationContract]
+    string listadoSolicitudesEnCamino();
+
+    [OperationContract]
+    List<Solicitud> listadoSolicitudesEmpresa(Empresa usLog);
+
+    #endregion
+}
---
+++ BiosLogistica/wcfLogistica/App_Code/Service.cs
@@ -0,0 +1,89 @@
+﻿using EntidadesCompartidas;
+using Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+    #region LogicaUsuario
+
+    void IService.AltaUsuario(Usuario usuario, Empleado usLog)
+    {
+        FabricaLogica.GetLogicaUsuario().AltaUsuario(usuario, usLog);
+    }
+
+    void IService.BajaUsuario(Usuario usuario, Empleado usLog)
+    {
+        FabricaLogica.GetLogicaUsuario().BajaUsuario(usuario, usLog);
+    }
+
+    Usuario IService.BuscarUsuario(string logueo, Empleado usLog)
+    {
+        return FabricaLogica.GetLogicaUsuario().BuscarUsuario(logueo, usLog);
+    }
+
+    Usuario IService.LogueoUsuario(string logueo, string contraseana)
+    {
+        return FabricaLogica.GetLogicaUsuario().LogueoUsuario(logueo, contraseana);
+    }
+
+    void IService.ModificarUsuario(Usuario usuario, Empleado usLog)
+    {
+        FabricaLogica.GetLogicaUsuario().ModificarUsuario(usuario, usLog);
+    }
+
+    List<Empresa> IService.ListarEmpresas(Empleado usuarioLogueado)
+    {
+        return FabricaLogica.GetLogicaUsuario().ListarEmpresas(usuarioLogueado);
+    }
+
+    #endregion
+
+    #region LogicaPaquete
+
+    void IService.AltaPaquete(Paquete paquete, Empleado usLog)
+    {
+        FabricaLogica.GetLogicaPaquete().AltaPaquete(paquete, usLog);
+    }
+
+    Paquete IService.BuscarPaquete(int codigo, Empleado usLog)
+    {
+        return FabricaLogica.GetLogicaPaquete().BuscarPaquete(codigo, usLog);
+    }
+
+    List<Paquete> IService.ListadoPaquetesSinSolicitud(Empleado usLog)
+    {
+        return FabricaLogica.GetLogicaPaquete().ListadoPaquetesSinSolicitud(usLog);
+    }
+
+    #endregion
+
+    #region LogicaSolicitud
+
+    void IService.AltaSolicitud(Solicitud solicitud, Empleado usLog)
+    {
+        FabricaLogica.GetLogicaSolicitud().AltaSolicitud(solicitud, usLog);
+    }
+
+    void IService.ModificarEstadoSolicitud(Solicitud solicitud, Empleado usLog)
+    {
+        FabricaLogica.GetLogicaSolicitud().ModificarEstadoSolicitud(solicitud, usLog);
+    }
+
+    string IService.listadoSolicitudesEnCamino()
+    {
+        return FabricaLogica.GetLogicaSolicitud().listadoSolicitudesEnCamino();
+    }
+    List<Solicitud> IService.listadoSolicitudesEmpresa(Empresa usLog)
+    {
+        return FabricaLogica.GetLogicaSolicitud().listadoSolicitudesEmpresa(usLog);
+    }
+
+    #endregion
+}
